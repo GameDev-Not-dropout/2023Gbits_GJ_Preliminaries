324e899597dda9925360556f29c2c84c88e1adec
快晴kaisei <huangfan9029@gmail.com>
2023-04-01 11:34:58 +0800

    完成了线与线之间的拖拽关系

--- Assets/Scripts/Input/DragHandler.cs
+++ Assets/Scripts/Input/DragHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using static UnityEditor.PlayerSettings;
 
 public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
@@ -13,35 +14,54 @@ public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
     public Transform playerTransform;
     public int handlerIndex;
 
-    bool hasTransition;
-    bool hasResume;
+    Vector3 GetHandlerScreenPoint => RectTransformUtility.WorldToScreenPoint(null, transform.position);
 
     private void Start()
     {
         mainCamera = Camera.main;
+        this.SaveHandlerData();
 
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+
+        #region 防止两条线之间出现卡顿，弹出来一点
         if (handlerIndex == 2)
         {
-            TransitionManager.Instance.handler2 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+            if (TransitionManager.Instance.handler2 >= TransitionManager.Instance.handler3 - 0.5f)
+                rectTransform.offsetMin = new Vector2(mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler2 - 0.2f, 0f, 0f)).x, -(Screen.height / 2));
         }
         if (handlerIndex == 3)
         {
-            TransitionManager.Instance.handler3 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
-        }
+            if (TransitionManager.Instance.handler2 + 0.5f >= TransitionManager.Instance.handler3)
+                rectTransform.offsetMin = new Vector2(mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler3 + 0.2f, 0f, 0f)).x, -(Screen.height / 2));
+        } 
+        #endregion
+        this.SaveHandlerData();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
-        Vector3 pos;
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.enterEventCamera, out pos);
-        rectTransform.offsetMin = new Vector2(pos.x, -(Screen.height / 2));
 
+        if (handlerIndex == 2)
+        {
+            if (mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler3 - 0.5f, 0f, 0f)).x > Input.mousePosition.x)
+                this.DraggingHandler(eventData);
+            else
+                rectTransform.offsetMin = new Vector2(mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler3 - 0.6f, 0f, 0f)).x, -(Screen.height / 2));
+
+        }
+        if (handlerIndex == 3)
+        {
+            if (mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler2 + 0.5f, 0f, 0f)).x < Input.mousePosition.x)
+                this.DraggingHandler(eventData);
+            else
+                rectTransform.offsetMin = new Vector2(mainCamera.WorldToScreenPoint(new Vector3(TransitionManager.Instance.handler2 + 0.6f, 0f, 0f)).x, -(Screen.height / 2));
+
+        }
 
         if (playerTransform.position.x < 10)    // 当前玩家在场景1
         {
@@ -49,11 +69,9 @@ public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
             {
                 Vector3 point = mainCamera.WorldToScreenPoint(playerTransform.position);
 
-                if (Input.mousePosition.x < point.x)
+                if (GetHandlerScreenPoint.x < point.x)
                 {
-                    TransitionManager.Instance.handler2 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
                     playerTransform.position = new Vector3(playerTransform.position.x + 20, playerTransform.position.y);
-                    //hasTransition = true;
 
                     return;
                 }
@@ -61,33 +79,26 @@ public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
         }
         if (playerTransform.position.x > 10 && playerTransform.position.x < 30)    // 玩家在场景2
         {
-            //if (hasTransition) return;
-
             if (handlerIndex == 3)      // 跳转到场景3
             {
                 Vector3 point = sceneCamera2.WorldToScreenPoint(playerTransform.position);
 
-                if (Input.mousePosition.x < point.x)
+                if (GetHandlerScreenPoint.x < point.x)
                 {
-                    TransitionManager.Instance.handler3 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
                     playerTransform.position = new Vector3(playerTransform.position.x + 20, playerTransform.position.y);
 
                     return;
-
                 }
             }
             if (handlerIndex == 2)      // 回到场景1
             {
                 Vector3 point = sceneCamera2.WorldToScreenPoint(playerTransform.position);
 
-                if (Input.mousePosition.x > point.x)
+                if (GetHandlerScreenPoint.x > point.x)
                 {
-                    TransitionManager.Instance.handler2 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
                     playerTransform.position = new Vector3(playerTransform.position.x - 20, playerTransform.position.y);
 
-                    //hasResume = true;
                     return;
-
                 }
             }
         }
@@ -97,34 +108,47 @@ public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
             {
                 Vector3 point = sceneCamera3.WorldToScreenPoint(playerTransform.position);
 
-                if (Input.mousePosition.x > point.x)
+                if (GetHandlerScreenPoint.x > point.x)
                 {
-                    TransitionManager.Instance.handler3 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
                     playerTransform.position = new Vector3(playerTransform.position.x - 20, playerTransform.position.y);
 
                     return;
-
                 }
             }
         }
+
+    }
+    /// <summary>
+    /// 线跟随鼠标
+    /// </summary>
+    private void DraggingHandler(PointerEventData eventData)
+    {
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.enterEventCamera, out pos);
+        rectTransform.offsetMin = new Vector2(pos.x, -(Screen.height / 2));
+        this.SaveHandlerData();
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
-        //hasTransition = false;
-        //hasResume = false;
-        if (handlerIndex == 2) 
+
+        this.SaveHandlerData();
+    }
+
+    /// <summary>
+    /// 转为世界坐标，方便角色移动时判断是否越界
+    /// </summary>
+    private void SaveHandlerData()
+    {
+        if (handlerIndex == 2)
         {
-            TransitionManager.Instance.handler2 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+            TransitionManager.Instance.handler2 = mainCamera.ScreenToWorldPoint(GetHandlerScreenPoint).x;
         }
         if (handlerIndex == 3)
         {
-            TransitionManager.Instance.handler3 = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+            TransitionManager.Instance.handler3 = mainCamera.ScreenToWorldPoint(GetHandlerScreenPoint).x;
         }
-        //canvasGroup.blocksRaycasts = true;
-
     }
-
 }
--- Assets/Scripts/Managers/TransitionManager.cs
+++ Assets/Scripts/Managers/TransitionManager.cs
@@ -7,17 +7,12 @@ public class TransitionManager : MonoBehaviour
 {
     public static TransitionManager Instance;
 
-    public float defaultHandler2;
-    public float defaultHandler3;
     [NonSerialized] public float handler2, handler3;
 
 
     private void Awake()
     {
         Instance = this;
-        Instance.handler2 = defaultHandler2;
-        Instance.handler3 = defaultHandler3;
-
     }
 
 
