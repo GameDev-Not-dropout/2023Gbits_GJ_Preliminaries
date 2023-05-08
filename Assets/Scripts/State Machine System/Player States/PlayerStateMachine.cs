68e9a67f5cfa1f1a29451cadeb1e8244662bbb28
kaisei <huangfan9029@Gmail.com>
2023-05-08 22:56:14 +0800

    重置重生逻辑，优化游戏体验
    添加了玩家的透明拷贝，防止以外跳转导致玩家找不着北

--- Assets/Scripts/State Machine System/Player States/PlayerStateMachine.cs
+++ Assets/Scripts/State Machine System/Player States/PlayerStateMachine.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine  // 挂载到玩家身上
 {
-    [SerializeField] PlayerState[] states;  
+    [SerializeField] PlayerState[] states;
+    public SpriteRenderer copyImage;
 
     Animator animator;
     PlayerController player;
     PlayerInput input;
+    SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         #region 状态需要绑定的组件
         animator = GetComponentInChildren<Animator>();
         player = GetComponent<PlayerController>();
-        input = GetComponent<PlayerInput>(); 
+        input = GetComponent<PlayerInput>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
         #endregion
 
         stateTable = new Dictionary<System.Type, IState>(states.Length);    // 初始化状态字典
@@ -29,5 +32,30 @@ public class PlayerStateMachine : StateMachine  // 挂载到玩家身上
     private void Start()
     {
         SwitchOn(stateTable[typeof(PlayerState_Idle)]);    // 状态机以空闲状态启动
+    }
+
+    public override void TimeUpdate()
+    {
+        base.TimeUpdate();
+        copyImage.sprite = spriteRenderer.sprite;
+
+        Vector3 pos = transform.position;
+        if (pos.x < 40)
+        {
+            if (transform.localScale.x < 0)
+                copyImage.transform.localPosition = new Vector3(-80, 0);
+            else
+                copyImage.transform.localPosition = new Vector3(80, 0);
+
+        }
+        else
+        {
+            if (transform.localScale.x < 0)
+                copyImage.transform.localPosition = new Vector3(80, 0);
+            else
+                copyImage.transform.localPosition = new Vector3(-80, 0);
+        }
+
+
     }
 }
